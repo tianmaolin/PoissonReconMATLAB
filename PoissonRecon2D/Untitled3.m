f27ccd8a03c69c9c68ec703bcc9ee8823f207929
tianmaolin <30468336+tianmaolin@users.noreply.github.com>
2018-04-09 00:44:24 +0800

    start for 3-D

---
+++ PoissonRecon2D/Untitled3.m
@@ -0,0 +1,29 @@
+% N = 400;
+% x = -1.5:3/N:1.5-1/N;
+% [x,y] = meshgrid(x,x);
+% z = zeros(size(x));
+% for i = 1:N
+%     for j = 1:N
+%         r = x(i,j)^2+y(i,j)^2;
+%         if r<=1.1 && r>=0.9
+%             z(i,j) = 0.5*(sin(5*(r-0.9)*pi+pi/2)+1);
+%         elseif r<0.9
+%             z(i,j) = 1;
+%         end
+%     end
+% end
+% mesh(x,y,z)
+
+N = 2^6;
+x = -1.5:3/N:1.5-1/N;
+[x,y] = meshgrid(x,x);
+z = zeros(size(x));
+for i = 1:N
+    for j = 1:N
+        r = x(i,j)^2+y(i,j)^2;
+        if r<=1.1 && r>=0.9
+            z(i,j) = sin(5*(r-0.9)*pi+pi/2) * 50*pi*pi*r-cos(5*(r-0.9)*pi+pi/2)*10*pi;
+        end
+    end
+end
+surf(z)
